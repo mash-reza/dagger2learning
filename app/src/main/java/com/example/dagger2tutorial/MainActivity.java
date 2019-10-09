884a1d305088c3d6c9d7f93cac7b8b8baa4dfd57
Festive <27864684+mash-reza@users.noreply.github.com>
2019-10-09 13:32:43 +0330

    video 3

--- app/src/main/java/com/example/dagger2tutorial/MainActivity.java
+++ app/src/main/java/com/example/dagger2tutorial/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        car = DaggerCarComponent.create().getCar();
+        DaggerCarComponent.create().inject(this);
         car.drive();
     }
 }
